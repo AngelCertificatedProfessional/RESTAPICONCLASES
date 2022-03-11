398f2d3c1f7702821c46c1ef3c953bdd78bf9666
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2022-03-10 16:59:31 -0800

    Se agrego el proceso para guardar los modelos  y la bd

--- models/server.js
+++ models/server.js
@@ -2,16 +2,26 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 
+const { dbConnection } = require('../database/config');
+
 class Server {
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT
         this.usuariosPath = '/api/usuarios';
+
+        //Conectar a base de datos
+        this.conectarDB();
+
         //Midlewares
         this.middlewares();
         //Rutas de mi aplicacion
         this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
     }
 
     middlewares(){
