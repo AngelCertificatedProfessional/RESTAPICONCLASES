cc22e135e4d6f814a8d5c10b72a14b9965d18615
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2022-03-11 16:57:28 -0800

    Se agrego el proceso de los contradores de usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,5 @@
 const { response } = require('express')
-
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req, res = response) => {
 
@@ -11,13 +11,14 @@ const usuariosGet = (req, res = response) => {
     })
 }
 
-const usuariosPost = (req, res = response) => {
-
+const usuariosPost = async (req, res = response) => {
     const body = req.body;
+    const usuario = new Usuario(body);
+    await usuario.save();
 
     res.json({
         msg:'post api - controllador',
-        body
+        usuario    
     })
 }
 
