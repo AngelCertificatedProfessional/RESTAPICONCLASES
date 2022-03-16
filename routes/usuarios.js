5eda6784daef7d8680224cd75d93eb5e1def8b40
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2022-03-15 23:30:28 -0700

    Se agregan cambios de usuario y helpers

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,15 +1,15 @@
 const {Router} = require('express')
 const {check} = require('express-validator')
-const Role = require('../models/role')
-
 
 const {validarCampos} = require('../middlewares/validar-campos')
+const {esRoleValido} = require('../helpers/db-validators');
 
 const { usuariosGet,
     usuariosPost,
     usuariosPut,
     usuariosPath,
     usuariosDelete } = require('../controllers/usuarios')
+
 
 const router = Router();
 
@@ -21,12 +21,7 @@ router.post('/',[
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     check('password','El password debe ser mas de 6 letras').isLength({min:6}),
     check('correo','El correo no es valido').isEmail(),
-    check('rol').custom( async (rol = '') => {
-        const existeRol = await Role.findOne({rol})
-        if(!existeRol){
-            throw new Error(`El rol ${rol} no esta registrado en la BD`);
-        }
-    }),
+    check('rol').custom( (rol) => esRoleValido(rol)),
     validarCampos
 ],usuariosPost);
 
